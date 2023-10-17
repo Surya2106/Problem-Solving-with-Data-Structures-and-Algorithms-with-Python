21eb1b4b1ac33fb03d26ddf4860ee56aeaaa1621
Surya Teja <32254219+Surya2106@users.noreply.github.com>
2023-10-17 22:15:03 +0530

    Update D27P1_LeetCode_1302.py
    Solution for single number using bit manipulation

--- Python Solutions/D27P1_LeetCode_1302.py
+++ Python Solutions/D27P1_LeetCode_1302.py
@@ -1,1 +1,11 @@
-# Python3 code coming soon
+class Solution(object):
+    def singleNumber(self, nums):
+        ans=0
+        for i in range(0,32):
+            count = 0
+            for j in range(len(nums)):
+                if (nums[j]>>i)&1:
+                    count+=1
+            if count%2==1:
+                ans+=(2**i)
+        return ans
